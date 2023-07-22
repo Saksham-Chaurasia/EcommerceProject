d4ae5cab696142f2bbd2e650859445529df4ffe6
Saksham Chaurasia <saksham84a@gmail.com>
2023-07-22 16:45:34 +0530

    airflow-successfull-dag-code

--- ecommerce_dag.py
+++ ecommerce_dag.py
@@ -6,7 +6,6 @@ from airflow.operators.dummy_operator import DummyOperator
 import pandas as pd
 
 
-
 default_args = {'owner': 'airflow',
         'depends_on_past': False,
         'start_date': datetime(2023,6,22),
@@ -32,7 +31,7 @@ with DAG(dag_id='ecommerce_datapipeline',
     
     sqoop_export_stage_purchase = BashOperator(
         task_id = 'stage_purchase',
-        bash_command='''sqoop export --connect jdbc:mysql://localhost/commerce \
+        bash_command='''/home/hadoop/sqoop/bin/sqoop export --connect jdbc:mysql://localhost/commerce \
             --username saksham --password password --table stage_purchase \
             --export-dir /companyProject/purchase_data.csv \
             --columns "userID,timestamp,amount" --input-fields-terminated-by ',' --input-escaped-by '\"' -m 4 '''
@@ -40,7 +39,7 @@ with DAG(dag_id='ecommerce_datapipeline',
 
     sqoop_export_stage_customer = BashOperator(
         task_id = 'stage_customer',
-        bash_command= ''' sqoop export --connect jdbc:mysql://localhost/commerce \
+        bash_command= ''' /home/hadoop/sqoop/bin/sqoop export --connect jdbc:mysql://localhost/commerce \
             --username saksham --password password --table stage_customer \
             --export-dir /companyProject/customer_data.csv \
             --columns "userID,name,email" --input-fields-terminated-by ',' --input-escaped-by '\"' -m 4'''
@@ -48,7 +47,7 @@ with DAG(dag_id='ecommerce_datapipeline',
 
     sqoop_export_stage_clickstream=BashOperator(
         task_id = 'stage_clickstream',
-        bash_command='''sqoop export --connect jdbc:mysql://localhost/commerce \
+        bash_command='''/home/hadoop/sqoop/bin/sqoop export --connect jdbc:mysql://localhost/commerce \
             --username saksham --password password --table stage_clickstream \
             --export-dir /companyProject/clickstream_data.csv \
             --columns "userID,timestamp,page" --input-fields-terminated-by ',' --input-escaped-by '\"' -m 4'''
@@ -56,52 +55,52 @@ with DAG(dag_id='ecommerce_datapipeline',
 
     sqoop_eval_purchase=BashOperator(
         task_id='purchase',
-        bash_command='''sqoop eval --connect jdbc:mysql://localhost/commerce \
+        bash_command='''/home/hadoop/sqoop/bin/sqoop eval --connect jdbc:mysql://localhost/commerce \
             --username saksham --password password --query "insert into purchase \
             select * from stage_purchase where userID !='userID';"'''
     )
 
     sqoop_eval_customer=BashOperator(
         task_id='customer',
-        bash_command='''sqoop eval --connect jdbc:mysql://localhost/commerce \
+        bash_command='''/home/hadoop/sqoop/bin/sqoop eval --connect jdbc:mysql://localhost/commerce \
             --username saksham --password password --query "insert into customer \
                 select * from stage_customer where userID !='userID';"'''
     )
 
     sqoop_eval_clickstream=BashOperator(
         task_id='clickstream',
-        bash_command='''sqoop eval --connect jdbc:mysql://localhost/commerce \
+        bash_command='''/home/hadoop/sqoop/bin/sqoop eval --connect jdbc:mysql://localhost/commerce \
             --username saksham --password password --query "insert into clickstream \
                 select * from stage_clickstream where userID !='userID';"'''
     )
 
     sqoop_import_purchase=BashOperator(
-        task_id='hive_purchase',
-        bash_command='''sqoop import --connect jdbc:mysql://localhost/commerce \
-            --username saksham --password password --table purchase \
-                --hive-table commerce.purchase --create-hive-table --hive-import -m1'''
+        task_id='hdfs_purchase',
+        bash_command='''/home/hadoop/sqoop/bin/sqoop import --connect jdbc:mysql://localhost/commerce \
+            --username saksham --password password --table purchase -m1 --target-dir /user/saksham/purchase_data'''
     )
-
+    
     sqoop_import_customer=BashOperator(
-        task_id='hive_customer',
-        bash_command='''sqoop import --connect jdbc:mysql://localhost/commerce \
-            --username saksham --password password --table customer --hive-table commerce.customer \
-                --create-hive-table --hive-import -m1'''
+        task_id='hdfs_customer',
+        bash_command='''/home/hadoop/sqoop/bin/sqoop import --connect jdbc:mysql://localhost/commerce \
+            --username saksham --password password --table customer -m1 --target-dir /user/saksham/customer_data'''
     )
 
     sqoop_import_clickstream=BashOperator(
-        task_id='hive_clickstream',
-        bash_command='''sqoop import --connect jdbc:mysql://localhost/commerce \
-            --username saksham --password password --table clickstream \
-                --hive-table commerce.clickstream --create-hive-table --hive-import -m1'''
+        task_id='hdfs_clickstream',
+        bash_command='''/home/hadoop/sqoop/bin/sqoop import --connect jdbc:mysql://localhost/commerce \
+            --username saksham --password password --split-by userID --table clickstream -m1 --target-dir /user/saksham/clickstream_data'''
     )
+
 
     end_purchase = DummyOperator(task_id='end_purchase')
     end_customer = DummyOperator(task_id='end_customer')
     end_clickstream = DummyOperator(task_id='end_clickstream')
 
-sqoop_export_stage_purchase >>sqoop_eval_purchase >> sqoop_import_purchase
+start_purchase >> sqoop_export_stage_purchase >>sqoop_eval_purchase >> sqoop_import_purchase >> end_purchase
 
-sqoop_export_stage_customer >> sqoop_eval_customer >> sqoop_import_customer
+start_customer >> sqoop_export_stage_customer >> sqoop_eval_customer >> sqoop_import_customer >> end_customer
 
-sqoop_export_stage_clickstream >> sqoop_eval_clickstream >> sqoop_import_clickstream
+start_clickstream >> sqoop_export_stage_clickstream >> sqoop_eval_clickstream >> sqoop_import_clickstream >> end_clickstream
+
+
